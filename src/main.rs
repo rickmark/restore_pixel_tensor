dc4159be31b3737d8c2d9b311f4560f2b48aa4be
rickmark <rickmark@outlook.com>
2024-09-18 23:25:09 -0700

    Baase version

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,70 @@
 extern crate argparse;
+extern crate hex;
+extern crate std;
+extern crate serialport;
 
-use argparse::{ArgumentParser, StoreOption};
-use xmodem::Xmodem;
-use std::fs;
-use std::fs::*;
-use serialport::{TTYPort, SerialPort};
+use argparse::{Store, ArgumentParser};
 use regex::Regex;
+use std::path::Path;
+use std::fs::File;
+use std::result::Result;
+use xmodem::Xmodem;
+use std::i64;
+use std::io::Read;
 
 const XMODEM_BAUD_EBM: u32 = 9600;
 
-const BL_REQUEST_REGEX: Regex = Regex::new(r"(?m)^\((\w+):0x([0-9A-F]+)\)$").unwrap();
-
 struct BootloaderCommand {
-    requested_payload: &str,
+    requested_payload: String,
+    direction: String,
     base_address: i64
 }
 
-fn parse_bootloader_command(command: &str) -> BootloaderCommand {
-    let groups = BL_REQUEST_REGEX.captures(command);
+fn parse_bootloader_command(command: &str) -> Result<BootloaderCommand, &str> {
+    let bl_request_regex: Regex = Regex::new(r"(?m)^eub:(req):([0-9A-F]+):(\w+)$").unwrap();
 
-    let mut result = BootloaderCommand {
-        requested_payload: groups[1],
-        base_address: groups[2]
-    };
+    let groups = bl_request_regex.captures(command);
 
-    return result;
+    match groups {
+        Some(g) => {
+            let result =                 BootloaderCommand {
+                direction: g[1].to_string(),
+                base_address: i64::from_str_radix(&g[2], 16).expect("Failed to parse base address"),
+                requested_payload: g[3].to_string(),
+            };
+            Ok(result)
+        },
+        None => {
+            Err("Unable to parse the line")
+        }
+    }
 }
 
 fn main() {
-    let mut parser = ArgumentParser::new();
+    let mut firmware = "".to_string();
+    let mut device = "".to_string();
+    {
+        let mut parser = ArgumentParser::new();
+        parser.refer(&mut firmware).add_option(&["-f", "--firmware"], Store, "The directory of the boot firmware");
+        parser.refer(&mut device).add_option(&["-d", "--device"], Store, "The UART device path");
+        parser.parse_args_or_exit();
+    }
 
-    let mut firmware: &str = "";
-    let mut device: &str = "";
-    parser.refer(&mut firmware).add_option(&["-f", "--firmware"], StoreOption<&str>, "The directory of the boot firmware");
-    parser.refer(&mut device).add_option(&["-d", "--device"], StoreOption<&str>, "The UART device path");
+    let firmware_path = Path::new(&firmware);
 
-    let firmwarePath = fs::read_dir(firmware);
+    let mut port = serialport::new(device, XMODEM_BAUD_EBM).open().expect("Port must open");
 
-    let (mut receivePort, mut sendPort) = serialport::new(device, XMODEM_BAUD_EBM).open().pair();
+    let mut input_string: String = "".to_string();
+    port.read_to_string(&mut input_string).expect("Failed to read");
 
-    let mut bootloaderCommand = parse_bootloader_command(receivePort.read());
+    let bootloader_command = parse_bootloader_command(&input_string).unwrap();
 
+    if bootloader_command.direction == "req" {
+        let file_path = firmware_path.join(Path::new(&format!("{}.bin", &bootloader_command.requested_payload)));
 
-    let mut xmodemConnection = Xmodem::new();
-    xmodemConnection.send(&port, &firmwareFile);
+        let mut file = File::open(file_path).expect("must open");
+
+        let mut xmodem_connection = Xmodem::new();
+        xmodem_connection.send(&mut port, &mut file).expect("TODO: panic message");
+    }
 }
-
-
-// (bl2:0xXXXXXXXX)incrementer
-// EBM request for payload of type bl2
-// load address?
-//
-//
